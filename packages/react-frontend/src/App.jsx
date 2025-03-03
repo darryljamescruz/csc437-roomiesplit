3a0b9a9e99e2f1f27bdb17f0c8e786a501f9654f
Darryl <darryljamescruz@gmail.com>
2025-03-02 20:55:31 -0800

    added signup page w/ routing

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 import MainLayout from './pages/MainLayout';
 import MainPage from './pages/Mainpage';
 import './index.css';
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route element={<MainLayout />}>
           <Route path="/main" element={<MainPage />} />
           {/* Additional protected routes can go here */}
