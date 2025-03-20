4eb99c9024c82148d3c4237129deccc88861378a
Darryl <darryljamescruz@gmail.com>
2025-03-19 22:41:45 -0700

    feat: add Purchase model with schema definition and TypeScript interface

---
+++ packages/express-backend/src/models/Purchase.ts
@@ -0,0 +1,29 @@
+// /packages/express-backend/src/models/Purchase.ts
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IPurchase extends Document {
+  date: string;
+  name: string;
+  cost: number;
+  category: string;
+  person: string;
+  assignees: string[];
+  household: mongoose.Types.ObjectId; // Reference to the associated household
+}
+
+const PurchaseSchema: Schema<IPurchase> = new Schema({
+  date: { type: String, required: true },
+  name: { type: String, required: true },
+  cost: { type: Number, required: true },
+  category: { type: String, required: true },
+  person: { type: String, required: true },
+  assignees: { type: [String], required: true },
+  household: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Household', 
+    required: true 
+  }
+});
+
+const Purchase = mongoose.model<IPurchase>('Purchase', PurchaseSchema);
+export default Purchase;
